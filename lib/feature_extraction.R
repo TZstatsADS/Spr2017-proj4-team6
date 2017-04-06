6d087354a02a70d54d4c833294c4d3073a6430fe
Galen Simmons <galen.simmons@gmail.com>
2017-04-06 13:07:10 -0400

    incremental update
    small changes to a few files

--- lib/feature_extraction.R
+++ lib/feature_extraction.R
@@ -5,10 +5,39 @@ ntf <- function(){
   
 }
 
+# create tibble from data frame
+createDocumentTermMatrix <- function(filePath){
+  require(tm,tidytext)
+  text <- read_csv(filePath)
+  docs <- as.vector(text$Paper)
+  source <- VectorSource(docs)
+  stop_words = stopwords(kind = "en")
+  corpus <- SimpleCorpus(source, control = list(language = "en",
+                                                removeWords = stop_words,
+                                                removeNumbers = TRUE,
+                                                stemming = TRUE))
+  dtm <- DocumentTermMatrix(corpus)
+  return(tidy(dtm))
+}
+
+countDocsUsingWord <- function(tdm){
+  require(dplyr)
+  ctdm <- tdm %>%
+    group_by(term) %>%
+    summarise(totwc = count(term))
+  return(ctdm)
+}
+
 # term frequency inverse document frequency
 tfidf <- function(tf,idf){
   # dot product of term frequency and inverse document frequency
-  
+  return(tf %*% idf)
+}
+
+# inverse document frequency
+idf <- function(docsCount, docsUsingWord){
+  idf <- log10(docsCount/(1+docsUsingWord))
+  return(idf)
 }
 
 # count number of times a citation appears
