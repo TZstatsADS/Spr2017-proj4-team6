7146787a89b25964e9f1a86fd87d020e83cd211a
amandazhang <amandazhang@berkeley.edu>
2017-04-07 15:06:57 -0400

    I still hate JLee

--- lib/Constraint_Function.R
+++ lib/Constraint_Function.R
@@ -17,3 +17,32 @@ AGupta <- read.csv("../output/Agupta.csv")
 
 c_2(6,7,AGupta)
 c_2(6,10,AGupta)
+
+
+
+# part 4.2 
+# Union set of all pi.authors with author named a.
+#---
+# input: dataframe of author named a
+#---
+union_author <- function(df) {
+  return(unique(unlist(strsplit(df$Coauthor, split = ";"))))
+}
+
+# Take AGupta for example, get union of unique values of authors.
+uni <- unique(unlist(strsplit(AGupta$Coauthor, split = ";")))
+
+# Matrix Mp
+M_p <- diag(1, nrow = nrow(df))
+M_pa <- function(df) {
+  authors <- union_author(df)
+  mat_pa <- matrix(NA, nrow = nrow(df), ncol = length(authors))
+  for (i in 1:nrow(mat_pa)) {
+    for (j in 1:ncol(mat_pa)) {
+      mat_pa[i, j] <- ifelse(grepl(authors[j], df$Coauthor[i])==T, 1, 0)
+    }
+  }
+  return(mat_pa)
+}
+
+x <- M_pa(AGupta)
