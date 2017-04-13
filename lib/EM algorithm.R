0524b7177877d0b3b9631f4c53b320472faded3f
gzy0553 <gzy0553@gmail.com>
2017-04-13 17:49:02 -0400

    Add files via upload

--- lib/EM algorithm.R
+++ lib/EM algorithm.R
@@ -1,5 +1,7 @@
 #EM algorithm
-
+#l is the matrix of all author
+#Y is the matrix of each paper's author
+df<-AGupta
 k<-max(df$AuthorID)
 n<-nrow(X)
 numb<-0
@@ -20,10 +22,13 @@ if(obj_func(X,Y,df)==obj_func(X,Y_stop,df)) break
 #M-step
 for (i in 1:k){
   sum_x<-0
+  c<-NULL
   for(j in 1:n){
     if (Y[j,]==l[i,]) sum_x<-sum_x+X[j,]
+    rbind(c,j)
   }
   l[i,]<-sum_x/sqrt(t(sum_x)%*%A%*%sum_x)
+  Y[c,]<-l[i,]
 }
 for(m in 1:n){
   A[m,m]<-A[m,m]+0.75*dif_func(X,Y,A,m)
