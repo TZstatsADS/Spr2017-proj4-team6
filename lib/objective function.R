b3d148be5f1a6b74a043bd63f0c5223954a9667f
gzy0553 <gzy0553@gmail.com>
2017-04-13 18:09:43 -0400

    Add files via upload

--- lib/objective function.R
+++ lib/objective function.R
@@ -1,15 +1,14 @@
 #objective function
 
-obj_func<-function(X,Y,df,l){
+obj_func<-function(X,Y,df){
   #X is the matrix of feature of all papers.
   #df is the list of all articles.
-  #l is the vector of current tags for all papers.
   #Y is the matrix of researchers for all papers.
   n<-nrow(X)
   sum<-0
   for (i in 1:n){
     for (j in i:n){
-      if (l[i]!=l[j]){
+      if (Y[i,]!=Y[j,]){
         sum<-sum+dist_feature(X[i,],X[j,],A)*
           (0.7*c_2(X[i,],X[j,],df)+c_6(x[i,],x[j,],df))
       }
